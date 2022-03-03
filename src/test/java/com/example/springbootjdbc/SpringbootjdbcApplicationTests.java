82942c43a8f2641f58e6e308a3febe0ede1b5fd5
satishsampate <sesampate@gmail.com>
2022-03-03 21:27:51 +0530

    Committed test class.

--- src/test/java/com/example/springbootjdbc/SpringbootjdbcApplicationTests.java
+++ src/test/java/com/example/springbootjdbc/SpringbootjdbcApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.springbootjdbc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// this is the test class.
 @SpringBootTest
 class SpringbootjdbcApplicationTests {
 
